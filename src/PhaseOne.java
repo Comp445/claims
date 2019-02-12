947664c49892247880c183c0c00ab2855e0ffb9d
Frank Tutino <fat.nat@videotron.ca>
2019-02-11 21:54:37 -0500

    added onefile and multifile to phase1, started phase2

--- src/PhaseOne.java
+++ src/PhaseOne.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import static java.util.stream.Collectors.*;
@@ -11,11 +12,12 @@ import static java.util.stream.Collectors.*;
 public final class PhaseOne {
 	 //variables
 	final int maxTupple;//holds the max number of tuple depending on heap size
-	final String fileName;
+	final String fileName; //files name from input
 	static String line;//buffer to hold line of the file
 	static List<String> buffer =  new ArrayList<String>(); //hold a list of CID and  Amount-paid as one string per tuple per
-	static int tuples = 0;//number of tuples to insert in buffer
-	static int fileCounter=0; //used to name and count files saved on disk
+	static int tuples = 1;//number of tuples to insert in buffer
+	static int fileCounter=0; /*used to name and count files saved on disk,
+	 							NOTE will hold one more than then actual number of files created because count starts at one but file name starts at 0 */
 	
 	//private constructor and final class all methods are static to simulate static class like in C++
 	private PhaseOne(int heapSize, String fileName){
@@ -25,12 +27,42 @@ public final class PhaseOne {
 	
 	//starts the phase one process
 	public static void start() {
-	try {//open input file to stream
-		BufferedReader file = Files.newBufferedReader(Paths.get("input.txt"));
+
+	try {
 		//will enter the loop if there's any data in file
+		System.out.println("Phase one started");
+		xFiles();	
+		//oneFile();
+      
+    } catch(IOException io) {
+       io.printStackTrace();
+    	}
+	}
+
+	private static void writeTo_xFile() throws IOException
+	{//sorts and writes to file
+		Path path = Paths.get("f"+fileCounter);
+		List<String> sortedBuffer =buffer.stream()
+			.sorted()
+			.collect(toList());
+		Files.write(path, sortedBuffer);
+		fileCounter++;
+	}
+	private static void writeTo_OneFile() throws IOException
+	{
+		List<String> sortedBuffer =buffer .stream()
+				.sorted()
+				.collect(toList());
+		Files.write(Paths.get("outPut.txt"),(Iterable<String>)sortedBuffer::iterator,StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+	}
+	//writes multi files of buffer size each
+	private static void xFiles() throws IOException
+	{
+		//open input file to stream
+		BufferedReader file = Files.newBufferedReader(Paths.get("input.txt"));
 		while((line = file.readLine()) != null )
 		{//this loop limits the buffer depending on heap size
-			while(line!=null && tuples <29999)
+			while(line!=null && tuples <45000)
 			{
 				buffer.add(line.substring(18,27)+ line.substring(241,250));//adds string to buffer
 				tuples++;//keeps count of tuples in buffer
@@ -39,28 +71,35 @@ public final class PhaseOne {
 			if(line!=null)//inserts last tuple in buffer if count is reached
 				buffer.add(line.substring(18,27)+ line.substring(241,250));
 			
-			writeToFile();//write the files in sorted order
-			tuples=0;//reset tuple count to zero
+			writeTo_xFile();//write the files in sorted order
+			tuples=1;//reset tuple count to zero
 			buffer.clear();//clear buffer to get it ready to fill again if necessary			
-		}		
-		file.close();
+		}
 		
-		
-      
-    } catch(IOException io) {
-       io.printStackTrace();
-    	}
-	}
-
-	private static void writeToFile() throws IOException
+	}		
+	//write one file of buffers using seek
+	private static void oneFile() throws IOException
 	{
-		Path path = Paths.get("f"+fileCounter);
-		List<String> sortedBuffer =buffer .stream()
-			.sorted()
-			.collect(toList());
-		Files.write(path, sortedBuffer);
-		fileCounter++;
+		//open input file to stream
+		BufferedReader file = Files.newBufferedReader(Paths.get("input.txt"));
+		while((line = file.readLine()) != null )
+		{
+			while(line!=null && tuples <400)
+			{
+				buffer.add(line.substring(18,27)+ line.substring(241,250));//adds string to buffer
+				tuples++;//keeps count of tuples in buffer
+				line = file.readLine();
+			}
+			if(line!=null)//inserts last tuple in buffer if count is reached
+				buffer.add(line.substring(18,27)+ line.substring(241,250));
+			
+			writeTo_OneFile();//write the files in sorted order
+			tuples=0;//reset tuple count to zero
+			buffer.clear();//clear buffer to get it ready to fill again if necessary	
+		}		
 	}
 	
+}//endof PhaseOne
+
+
 	
-}
--- src/PhaseTwo.java
+++ src/PhaseTwo.java
@@ -1,9 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 //class that runs second phase of TPMMS
-
 public final class PhaseTwo {
+	//variables
+	static List<BufferedReader> filePointers = new ArrayList<BufferedReader>();//used to hold pointers to file
+	static List<String> buffer =  new ArrayList<String>(); //hold a string value from each file
+	
+	
 	//private constructor and final class all methods are static to simulate static class like C++
 	private PhaseTwo() {
 	}
+	
+	public static void start() 
+	{//holds a pointer to each file and write one big file in sorted order
+		System.out.println("Phase two started");
+		try {
+			xFiles();
+			System.out.println(buffer);		
+					
+		}catch(IOException io) {
+		       io.printStackTrace();
+		    }	
 
-}
+	}//end of start
+	
+	private static void xFiles() throws IOException
+	{
+		boolean flag = true; //used to see if all files created were merged
+		int start=0;
+		int end = PhaseOne.fileCounter<=150 ? PhaseOne.fileCounter:150;
+		int passes = 0;
+		
+		while(flag)
+		{//create pointer to each file created in phase one up to heap size
+			for(int i=start; i<end;i++)
+			{
+				filePointers.add(Files.newBufferedReader(Paths.get("f"+i)));
+				buffer.add(filePointers.get(i).readLine());
+			}
+			//merge files from buffer
+			
+			//will go around again if not fully merged
+			flag = end < PhaseOne.fileCounter ? true:false;
+			start = end;
+			end = end+150 <PhaseOne.fileCounter ? end+150: PhaseOne.fileCounter;
+			passes++;
+			buffer.clear();
+		}
+	
+		System.out.println(passes);
+	}
+	
+	
+	
+}//end of class
